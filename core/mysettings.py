2ccd95eca581a4944736d214a8091ee3b8e10e52
dongwoo2 <ieiedong@naver.com>
2024-05-09 19:24:01 +0900

    vs에서 도커 ignore 테스트

---
+++ core/mysettings.py
@@ -0,0 +1,12 @@
+DATABASES = {
+    'default':{
+        'ENGINE':'django.db.backends.mysql',
+        'NAME':'alcolholic',
+        'USER':'root',
+        'PASSWORD':'4106',
+        'HOST':'127.0.0.1',
+        'PORT':'3306',
+    }
+}
+
+SECRET_KEY = 'django-insecure-m@%18%dn@s1n5tz0mh2_1+kbr153c&o+-@3)r51t$qlo2sgl4%'
